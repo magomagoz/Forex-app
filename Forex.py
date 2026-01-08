94cc04732a6e2ed22ca5fa083f683ded98623c65
magomagoz <magostinienrico@gmail.com>
2026-01-09 00:26:13 +0100

    Update Forex.py

--- Forex.py
+++ Forex.py
@@ -282,22 +282,10 @@ if df_rt is not None and not df_rt.empty:
     # Candele
     fig.add_trace(go.Candlestick(x=p_df.index, open=p_df['open'], high=p_df['high'], 
                                  low=p_df['low'], close=p_df['close'], name='Prezzo'), row=1, col=1)
-    
-    # 1. Linea Superiore (Upper BB)
-    fig.add_trace(go.Scatter(x=p_df.index, y=p_df[c_u], 
-                             line=dict(color='rgba(173, 216, 230, 0.3)', width=1), 
-                             name='Upper BB'), row=1, col=1)
-    
-    # 2. LINEA MEDIA (Aggiunta qui)
-    fig.add_trace(go.Scatter(x=p_df.index, y=p_df[c_m], 
-                             line=dict(color='rgba(255, 255, 255, 0.5)', width=1.5, dash='dash'), 
-                             name='Middle BB (SMA 20)'), row=1, col=1)
-    
-    # 3. Linea Inferiore (Lower BB) e riempimento
-    fig.add_trace(go.Scatter(x=p_df.index, y=p_df[c_l], 
-                             line=dict(color='rgba(173, 216, 230, 0.3)', width=1), 
-                             fill='tonexty', fillcolor='rgba(173, 216, 230, 0.05)',
-                             name='Lower BB'), row=1, col=1)
+    # Bande Bollinger
+    fig.add_trace(go.Scatter(x=p_df.index, y=p_df[c_up], line=dict(color='rgba(173, 216, 230, 0.4)', width=1), name='Upper BB'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=p_df.index, y=p_df[c_mid], line=dict(color='rgba(255, 255, 255, 0.3)', width=1), name='Middle BB'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=p_df.index, y=p_df[c_low], line=dict(color='rgba(173, 216, 230, 0.4)', width=1), fill='tonexty', name='Lower BB'), row=1, col=1)
 
     # RSI
     fig.add_trace(go.Scatter(x=p_df.index, y=p_df['rsi'], line=dict(color='#ffcc00', width=2), name='RSI'), row=2, col=1)
